321fd45e5660f02c7fa70445f612bdbebdf890f2
Luna Daniela Linares Avila <lunisnanita12@gmail.com>
2026-02-28 01:38:26 -0500

    [add]: variables contantes_

--- Modulo1/Class-1.java
+++ Modulo1/Class-1.java
@@ -2,7 +2,20 @@
 public class Class1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Hello, World!");
+
+        //variables Constantes
+
+        final double CM_POR_METRO = 100.0;
+        final double LIBRAS_POR_KG = 2.20462;
+        final float FACTOR_CELSIUS_FAHRENHEIT = 1.8f;
+        final double OFFSET_FAHRENHEIT = 32.0;
+
+        System.out.println("Hello, World Welcome to ConvertDay!");
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, %s ! Welcome to ConvertDay!", name);
+
+
+       
     }
 }
