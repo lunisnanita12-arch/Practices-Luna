0919ec7e47f325799eec6cce213a5d5bc51281d9
Luna Daniela Linares Avila <lunisnanita12@gmail.com>
2026-03-11 00:31:05 -0500

    [add]:Ejercio banco/ funcion de -login() completada/fucion con parametros -saludar()

--- Modulo1/Class3/ejercicioBanco.java
+++ Modulo1/Class3/ejercicioBanco.java
@@ -11,25 +11,53 @@ public static final Scanner entrada= new Scanner(System.in);
     }
 public static void login() {
     
-String usuario = "luna";    
-int contraseña = 1234;
-boolean login = false;
-while (login == false) {
+        int usuario = 1415;    
+        int contraseña = 1234;
+        boolean login = false;
+        int intentos = 0;
+        while (login == false && intentos < 3) {
 
-        System.out.println("----L-O-G-I-N---");
-        System.out.println("Ingrese el Usuario");
-        String usuarioIngresado = entrada.nextLine();
-        System.out.println("Ingresa la Contraseña");
-        int contraseñaIngresada = entrada.nextInt();
+                System.out.println("----L-O-G-I-N---");
+                System.out.println("Ingrese el Usuario");
+                int usuarioIngresado = entrada.nextInt();
+                System.out.println("Ingresa la Contraseña");
+                int contraseñaIngresada = entrada.nextInt();
+                entrada.nextLine();
+            
+                if (usuario != usuarioIngresado || contraseña != contraseñaIngresada){
+                    System.out.println(" El usuario o la Contraseña es Incorrecta/ intentalo de Nuevo");
+                    intentos ++;}
 
+                    else
+                    {
+                        login = true;
+                       
+                    }
 
-        if (usuario != usuarioIngresado && contraseña != contraseñaIngresada){
-            System.out.println(" El usuario o la Contraseña es Incorrecta/ intentalo de Nuevo");
-        } else {
-            login = true;
-        }
+         }       
+        
+            if (intentos == 3){
+                System.out.println("Has excedido el numero de intentos");
+                                    
+                  }
+                   
+            else{
+                 login = true;
+                 saludar(usuario);
+                 cuenta();
+                }
+
+        
+        
 }
 
-System.out.println("Usuario Registrado.....");
+public static void saludar (int usuario){
+     System.out.printf("Bienvido a tu Cuenta %d%n", usuario);
 }
+/* 
+public static void cuenta(){
+System.out.println("Bienvenido a tu Cuenta luna");
+System.out.println("Digite del ");
+}
+*/
 }
