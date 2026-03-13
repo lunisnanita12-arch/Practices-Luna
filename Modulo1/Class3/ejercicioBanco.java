3edb6afedf289e881f38efca6a01c40872c10897
Luna Daniela Linares Avila <lunisnanita12@gmail.com>
2026-03-12 22:17:26 -0500

    [add]:Ejercicio banco/terminado

--- Modulo1/Class3/ejercicioBanco.java
+++ Modulo1/Class3/ejercicioBanco.java
@@ -46,7 +46,6 @@ public static void login() {
                    
             else{
                  login = true;
-                 saludar(usuario);
                  menu();
                 }
 
@@ -54,35 +53,89 @@ public static void login() {
         
 }
 
+// Metodo de impresion de nombre de usuario
 public static void saludar (int usuario){
      System.out.printf("Bienvido a tu Cuenta %d%n", usuario);
 }
 
+// Metodo de Consultar Saldo
+public static void consultarSaldo() {
+    System.out.println("Su saldo actual es: " + saldoInicial);
+}
+
+public static void transferirDinero() {
+    System.out.println("Digite el numero de Cuenta de Destino: ");
+    int numeroDestino = entrada.nextInt();
+    System.out.println("Digite el monto a transferir: ");
+    double montoDeTransferencia = entrada.nextInt();
+    entrada.nextLine();
+
+    if (montoDeTransferencia <= 0){
+        System.out.println("Monto Invalido");
+    } 
+    else if (saldoInicial < montoDeTransferencia){
+        System.out.println("Saldo insuficiente");
+
+    } else {
+        double saldoActual = saldoInicial - montoDeTransferencia;
+        saldoInicial = saldoActual; 
+        System.out.println("Transferencia realizada con exito");
+        System.out.printf("%n Saldo Restante: $ %.0f ", saldoInicial);    
+    
+    }   
+}
+
+public static void retirarDinero() {
+   
+    System.out.println("Digite el monto a retirar: ");
+    double montoDeTransferencia = entrada.nextInt();
+    entrada.nextLine();
+
+    if (montoDeTransferencia <= 0){
+        System.out.println("Monto Invalido");
+    } 
+    else if (saldoInicial < montoDeTransferencia){
+        System.out.println("Saldo insuficiente");
+
+    } else {
+        double saldoActual = saldoInicial - montoDeTransferencia;
+        saldoInicial = saldoActual; 
+        System.out.println("Retiro realizada con exito");
+        System.out.printf("%n Saldo Restante: $ %.0f ", saldoInicial);    
+    
+    }   
+}
+
+
+//Menu del Programa
 public static void menu(){
 saludar(usuario);
 int opcion;
 do{
-System.out.println("-------M---E---N---U------");
+System.out.println("\n-------M---E---N---U------");
 System.out.println("\n 1. Consultar Saldo");
 System.out.println(" 2. Transferir dinero");
 System.out.println(" 3. Retirar Dinero");
-System.out.println(" 4. Salir"); 
+System.out.println(" 4. Salir.\n"); 
 
     opcion = entrada.nextInt();
     switch (opcion){
-     case 1 ->{ 
-        System.out.println("ll")  ;                  
-            //consultarSaldo(); 
+     case 1 ->{      
+            consultarSaldo(); 
 
 
      }
      case  2->{
+        transferirDinero();
      }
      case  3->{
-       //retirarDinero();
+       retirarDinero();
      }
-     case  4-> {
+     case  4-> { 
+            System.out.println("Gracias por usar Banco DevSenior");
+            System.out.printf("Saldo final: $.0f");
             System.out.println("Saliendo de la cuenta.... Hasta Pronto!!");
+           
      }
      default ->{
         System.out.println("Valor Erroneo, intente de Nuevo");
