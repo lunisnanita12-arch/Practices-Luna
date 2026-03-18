e5c2e29fcfabca3f8885be2903dde5344853d928
Luna Daniela Linares Avila <lunisnanita12@gmail.com>
2026-03-18 13:06:06 -0500

    [add]: Se realizo todo el proyecto/ pendiente funcion de reporte de Mantenimiento

--- Modulo1/Class3/sistemaIndustrial/src/App.java
+++ Modulo1/Class3/sistemaIndustrial/src/App.java
@@ -7,102 +7,108 @@ public class App {
 // variables de Usuario 
     public static int codigoEmpleado = 4567;
     public static int turnoPermitido;
-    public static int materiaPrima = 500;
-    public static double materiaPrimaxPieza = 25;
+    public static int stockMateriaPrima = 500; //500kg
+    public static int numeroPiezaRealizadas = 0; // por 1 pieza necesita 25kg de materia prima
     public static int producionMaximaxTurnos = 20; // 20 piezas
     public static Scanner entradas = new Scanner(System.in);
 
 public static void login() {
 
     boolean login = false; 
+    int intentos = 0;
 
   do{  
         System.out.println("Ingrese su codigo de empleado: ");
         int codigoIngresado = entradas.nextInt();
             if (codigoIngresado != codigoEmpleado){
                 System.out.println("El Codigo Ingresado es Erroneo");
-            }else {
+                intentos ++;
+                System.out.println(intentos);
+            }
+            else{
                 
                     System.out.println("Ingrese el numero de turno que ingresa (1,2 o 3)");
                     int turnopermitido = entradas.nextInt();
                     switch (turnopermitido){
                         case 1,2,3 ->{
                             System.out.println("Has ingresado Exitosamente");
-                             login = true;
-                           
+                            login = true;
+                            menu();
                         }
                         default ->{
                             System.out.println(" El turno es Invalido intente nuevamente");
                         } 
                     }}
-        }while (login != true);
-         menu();
+
+
+               if (intentos == 3){
+                  System.out.println("El numero de intentos se ha excedido\n Acceso denegado");
+               }     
+        }while (login != true && intentos != 3);
         entradas.close();
 }
 
-public static void menu() {
-    int opcion;
-
-    do{
-
-    System.out.println("""
-
-        1. Producir piezas
-        2. Ver stock de materia prima
-        3. Reportar daño de máquina
-        4. Cerrar turno
-        
-        """);   
-        opcion = entradas.nextInt();
-        switch (opcion){
-                //producir pieza
+   public static void menu() {
+      int opcion;
+      do {
+         System.out.println("\n1. Producir piezas\n2. Ver stock de materia prima\n3. Reportar daño de máquina\n4. Cerrar turno\n\n");
+         opcion = entradas.nextInt();
+         switch (opcion) {
             case 1 ->{
-                producirPieza();
-            }
-              //Ver stock de materia prima
+               //producirpieza
+               producirPieza();}
             case 2->{
-                //Ver stock de materia prima
-            }
-            //  Reportar daño de máquina
+               verStock();
+            } 
+            // reportar daño
             case 3->{
-                // Reportar daño de máquina
+                  //pendiente esta funcion 
             }
-            //Cerrar turno
+            //cierre de turno
             case 4->{
-                //Cerrar turno
+               cierreTurno(numeroPiezaRealizadas, stockMateriaPrima);
+               break;
             }
-            default ->{
-                System.out.println(" Opcion Invalida");
+            default->{
+               System.out.println(" Opcion Invalida");
             }
-        }
-        }while (opcion != 4); 
-}
-public static void producirPieza() {
-    System.out.println("\n-----P-r-o-d-u-c-i-o-n---d-e---p-i-e-z-a----");
-    System.out.println("\n\n Digite el numero de piezas a fabricar:");
-    int numeroPieza = entradas.nextInt();
-    entradas.nextLine();
+         }
+      } while(opcion != 4);
 
-    if (numeroPieza < 0){
-        System.out.println("Las piezas deben ser mayores que 0");}
-    else if(numeroPieza > producionMaximaxTurnos){
-            System.out.println(" No debe superar 20 piezas");}
-    else if(numeroPieza < materiaPrimaxPieza){
-        System.out.println("Materia prima insuficiente");}
+   }
 
-        else {
-            double materiaNecesaria = numeroPieza * 25;      }
-            System.out.println("""
-                    Producción realizada con éxito
-                    Stock restante: ___ kg
-                    """);
-    
+   public static int producirPieza() {
+      System.out.println("\n-----P-r-o-d-u-c-i-o-n---d-e---p-i-e-z-a----");
+      System.out.println("\n\n Digite el numero de piezas a fabricar:");
+      int numeroPieza = entradas.nextInt();
+      entradas.nextLine();
+      if (numeroPieza < 0) {
+         System.out.println("Las piezas deben ser mayores que 0");
+      } else if (numeroPieza > producionMaximaxTurnos) {
+         System.out.println(" No debe superar 20 piezas");
+      } else if (numeroPieza*25 > stockMateriaPrima) {
+         System.out.println("Materia prima insuficiente");
+      } else {
+         numeroPiezaRealizadas += numeroPieza;
+         int materiaNecesaria = numeroPieza * 25;
+         stockMateriaPrima -= materiaNecesaria; 
+         System.out.printf("Producción realizada con éxito\nStock restante: %d kg\n", stockMateriaPrima);
+      }
+      return stockMateriaPrima;
 
-        
-    
+   }
 
-}
+   public static void verStock() {
+      System.out.printf("Stock de materia prima: %d", stockMateriaPrima);
+      
+   }
 
-
-
+   public static void cierreTurno(int piezasrealizadas, int stockMateriaPrima) {
+      System.out.printf("""
+            Total de piezas producidas: %d
+            Materia prima restante: %d
+            Estado final de la máquina: 
+            """, piezasrealizadas,stockMateriaPrima);
+      
+   }
 }   
